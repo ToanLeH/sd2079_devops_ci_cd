89117019d107a6d234bc48a474b52ce3ea0d8a58
root <root@VNNOT01969.localdomain>
2023-08-01 14:20:45 +0700

    restructure build folder

---
+++ cd/backend/vars/releaseBackEndPipeline.groovy
@@ -0,0 +1,52 @@
+#!/usr/bin/env groovy
+void call(Map pipelineParams) {
+
+    pipeline {
+
+        agent any
+
+        options {
+            disableConcurrentBuilds()
+            disableResume()
+            timeout(time: 5, unit: 'MINUTES')
+        }
+        
+        stages {
+            stage ('Release Backend') {
+                when {
+                    allOf {
+                        // Branch Event: Nornal Flow
+                        anyOf {
+                            branch 'main'
+                            branch 'PR-*'
+                        }
+                        allOf {
+                            changeset "**/vars/.cd/backend/**"
+                        }
+                    }
+                }
+                steps {
+                    script {
+                        releaseBackEnd()
+                    }
+                }
+            }
+        }
+
+        post {
+            cleanup {
+                cleanWs()
+            }
+        }
+    }
+}
+//========================================================================
+// node pipeline
+// Version: v1.0
+// Updated:
+//========================================================================
+//========================================================================
+// Notes:
+//
+//
+//========================================================================
