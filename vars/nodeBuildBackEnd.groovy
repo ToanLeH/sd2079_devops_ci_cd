9abc1d8dc81cf3a9f48a6fbbcd7b858455b6a885
root <root@VNNOT01969.localdomain>
2023-10-23 22:09:35 +0700

    add Trivy scan steps

--- vars/nodeBuildBackEnd.groovy
+++ vars/nodeBuildBackEnd.groovy
@@ -40,7 +40,7 @@ void call() {
     }
 
     stage ("Trivy Scan") {
-        steps {
+        script {
             // Install trivy
             sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3'
             sh 'curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl > html.tpl'
