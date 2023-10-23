db84afae3f054b1a17b3be84c764ca2498d8aaa4
root <root@VNNOT01969.localdomain>
2023-10-23 22:59:12 +0700

    add Trivy scan steps

--- vars/nodeBuildFrontEnd.groovy
+++ vars/nodeBuildFrontEnd.groovy
@@ -48,7 +48,7 @@ void call() {
             // Scan all vuln levels
             sh "mkdir -p reports"
             sh "wget https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl"
-            sh "trivy filesystem --ignore-unfixed --vuln-type os,library --format template --template 'html.tpl' -o reports/nodjs-scan.html ${WORKSPACE}/src/${buildFolder}"
+            sh "trivy filesystem --ignore-unfixed --vuln-type os,library --format template --template @./html.tpl -o reports/nodjs-scan.html ${WORKSPACE}/src/${buildFolder}"
             publishHTML target : [
                 allowMissing: true,
                 alwaysLinkToLastBuild: true,
